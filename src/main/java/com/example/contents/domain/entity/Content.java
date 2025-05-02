f5ea3651f691fd11e991141d6b397a20a9df5548
iamseung <seesungseok@naver.com>
2025-05-02 16:35:54 +0900

    ✨ 추가 도메인 작업

---
+++ src/main/java/com/example/contents/domain/entity/Content.java
@@ -0,0 +1,32 @@
+package com.example.contents.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@Table(name = "contents")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Content {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+
+    @Column(nullable = false)
+    private boolean isAdult;
+
+    @Column(nullable = false)
+    private boolean isFree;
+    private LocalDateTime eventStartAt;
+    private LocalDateTime eventEndAt;
+}
