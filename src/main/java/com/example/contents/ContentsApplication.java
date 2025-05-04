a2f64d3123b76698568d497e1947860ac30316e6
iamseung <seesungseok@naver.com>
2025-05-04 22:14:56 +0900

    ✨ 캐시 설정

--- src/main/java/com/example/contents/ContentsApplication.java
+++ src/main/java/com/example/contents/ContentsApplication.java
@@ -2,8 +2,10 @@ package com.example.contents;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ContentsApplication {
 
     public static void main(String[] args) {
